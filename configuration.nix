c83304a378696bb8e7732c4cdff641edc91f1c92
Robin Kumar <robinmogha@outlook.com>
2026-08-01 13:13:02 +0530

    Refactor NixOS configuration for clarity and updates

--- configuration.nix
+++ configuration.nix
@@ -1,253 +1,381 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# /etc/nixos/configuration.nix
 
 { config, lib, pkgs, zen-browser, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-    ];
+imports = [
+./hardware-configuration.nix
+];
 
+# ------------------------------------------------------------
 
-#showing password feedback 
-  security.sudo = { 
-    enable = true;
-    extraConfig = ''
-      Defaults pwfeedback
-      '';
-  };
+# Nix
 
-  #gnome stuff 
-  services.gnome.gnome-keyring.enable = true;
-  services.thumbnailer.enable = true;
-#darkmode
-# Add this block to your configuration.nix
-  programs.dconf.enable = true;  # You already have this
+# ------------------------------------------------------------
 
-# Force GTK apps to use dark theme
-    environment.sessionVariables = {
-      GTK_THEME = "Adwaita:dark";
-      XCURSOR_THEME = "Adwaita";
-      XCURSOR_SIZE = "27";
-    };
+nix.settings.experimental-features = [
+"nix-command"
+"flakes"
+];
 
+nixpkgs.config.allowUnfree = true;
 
-#new kernels
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+# ------------------------------------------------------------
 
-  boot.loader.systemd-boot.enable = false;
+# System
 
-  boot.loader.efi = {
-    canTouchEfiVariables = true;
-    efiSysMountPoint = "/boot/efi";
-  };
+# ------------------------------------------------------------
 
-  boot.loader.grub = {
-    enable = true;
-    efiSupport = true;
-    device = "nodev";
-    useOSProber = true;
-  };
+networking.hostName = "transcendent";
+networking.networkmanager.enable = true;
 
+time.timeZone = "Asia/Kolkata";
 
-#laptop stuff
-  services.logind.settings.Login = {
-    HandleLidSwitch = "ignore";
-    HandleLidSwitchExternalPower = "ignore";
-  };
+i18n.defaultLocale = "en_IN";
 
-
-programs.fish.enable = true;
-  environment.shells = with pkgs; [ pkgs.fish ];
-
-  users.users.isandrin = {
-    shell = pkgs.fish;
-  };
-
-
-#hyprland block 
-programs.hyprland = { 
-  enable = true;
-  xwayland.enable = true;
+i18n.extraLocaleSettings = {
+LC_ADDRESS = "en_IN";
+LC_IDENTIFICATION = "en_IN";
+LC_MEASUREMENT = "en_IN";
+LC_MONETARY = "en_IN";
+LC_NAME = "en_IN";
+LC_NUMERIC = "en_IN";
+LC_PAPER = "en_IN";
+LC_TELEPHONE = "en_IN";
+LC_TIME = "en_IN";
 };
 
+# ------------------------------------------------------------
+
+# Bootloader and kernel
+
+# ------------------------------------------------------------
+
+boot.kernelPackages = pkgs.linuxPackages_latest;
+
+boot.loader = {
+systemd-boot.enable = false;
+
+```
+efi = {
+  canTouchEfiVariables = true;
+  efiSysMountPoint = "/boot/efi";
+};
+
+grub = {
+  enable = true;
+  efiSupport = true;
+  device = "nodev";
+
+  # Keep this enabled only when dual-booting.
+  useOSProber = true;
+};
+```
+
+};
+
+# Keep this only if it fixes Intel display flickering,
+
+# freezing, or other PSR-related issues.
+
+boot.kernelParams = [
+"i915.enable_psr=0"
+];
+
+# ------------------------------------------------------------
+
+# User
+
+# ------------------------------------------------------------
+
+users.users.isandrin = {
+isNormalUser = true;
+description = "isandrin";
+shell = pkgs.fish;
+
+```
+extraGroups = [
+  "networkmanager"
+  "wheel"
+  "docker"
+  "libvirtd"
+  "kvm"
+];
+
+packages = [ ];
+```
+
+};
+
+# ------------------------------------------------------------
+
+# Shell
+
+# ------------------------------------------------------------
+
+programs.fish.enable = true;
+
+environment.shells = [
+pkgs.fish
+];
+
+environment.variables = {
+EDITOR = "nvim";
+VISUAL = "nvim";
+};
+
+# ------------------------------------------------------------
+
+# Security
+
+# ------------------------------------------------------------
+
+security.sudo = {
+enable = true;
+
+```
+extraConfig = ''
+  Defaults pwfeedback
+'';
+```
+
+};
+
+security.rtkit.enable = true;
+
+# ------------------------------------------------------------
+
+# Desktop environment
+
+# ------------------------------------------------------------
+
+programs.hyprland = {
+enable = true;
+xwayland.enable = true;
+};
+
+programs.dconf.enable = true;
+
+services.displayManager.sddm = {
+enable = true;
+wayland.enable = true;
+};
+
+services.gnome.gnome-keyring.enable = true;
+services.tumbler.enable = true;
+services.gvfs.enable = true;
 
 
-#dbus-thing-block
-  services.dbus.enable = true;
+# ------------------------------------------------------------
 
-  environment.variables = {
-    EDITOR = "nvim";
-    VISUAL = "nvim";
+# XDG portals
+
+# ------------------------------------------------------------
+
+xdg.portal = {
+enable = true;
+
+```
+extraPortals = with pkgs; [
+  xdg-desktop-portal-hyprland
+  xdg-desktop-portal-gtk
+];
+```
+
+};
+
+# ------------------------------------------------------------
+
+# AppImage and Flatpak
+
+# ------------------------------------------------------------
+
+programs.appimage = {
+enable = true;
+binfmt = true;
+};
+
+services.flatpak.enable = true;
+
+# ------------------------------------------------------------
+
+# Laptop and power management
+
+# ------------------------------------------------------------
+
+services.logind.settings.Login = {
+HandleLidSwitch = "ignore";
+HandleLidSwitchExternalPower = "ignore";
+};
+
+services.tlp.enable = true;
+services.acpid.enable = true;
+
+# ------------------------------------------------------------
+
+# Keyboard remapping
+
+# ------------------------------------------------------------
+
+services.keyd = {
+enable = true;
+
+```
+keyboards.default = {
+  ids = [ "*" ];
+
+  settings.main = {
+    rightalt = "leftmeta";
   };
+};
+```
 
-#app-image support don't forget
-  programs.appimage.enable = true;
-  programs.appimage.binfmt = true;
+};
 
+# ------------------------------------------------------------
 
-# xdg-desktop-portal (correct option name)
-  xdg.portal = { 
-    enable = true;
-    extraPortals = with pkgs; [ 
-      xdg-desktop-portal-hyprland 
-      xdg-desktop-portal-gtk
-    ];
+# Bluetooth
+
+# ------------------------------------------------------------
+
+hardware.bluetooth.enable = true;
+services.blueman.enable = true;
+
+systemd.user.services.blueman-applet.enable = false;
+
+# ------------------------------------------------------------
+
+# Audio
+
+# ------------------------------------------------------------
+services.pipewire = {
+enable = true;
+
+```
+alsa = {
+  enable = true;
+  support32Bit = true;
+};
+
+pulse.enable = true;
+jack.enable = true;
+```
+
+};
+
+# ------------------------------------------------------------
+
+# Containers and virtualization
+
+# ------------------------------------------------------------
+
+virtualisation.docker.enable = true;
+
+virtualisation.libvirtd.enable = true;
+
+# ------------------------------------------------------------
+
+# X11 keyboard layout
+
+# ------------------------------------------------------------
+
+services.xserver.xkb = {
+layout = "us";
+variant = "";
+};
+
+# ------------------------------------------------------------
+
+# System packages
+
+# ------------------------------------------------------------
+
+environment.systemPackages = with pkgs; [
+# Development
+gcc
+gnumake
+cmake
+gdb
+git
+neovim
+
+```
+# Terminal tools
+bc
+curl
+wget
+unzip
+tmux
+stow
+aria2
+
+# Desktop utilities
+brightnessctl
+cliphist
+libnotify
+wl-clipboard
+networkmanagerapplet
+
+# Media and thumbnails
+ffmpegthumbnailer
+
+# System tools
+lm_sensors
+
+# GTK support
+glib
+gsettings-desktop-schemas
+adwaita-icon-theme
+
+  (zen-browser.packages.${pkgs.system}.default.overrideAttrs (oldAttrs: {
+    buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgs.libpulseaudio ];
+
+    preFixup = (oldAttrs.preFixup or "") + ''
+      gappsWrapperArgs+=(
+        --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath [ pkgs.libpulseaudio ]}"
+      )
+    '';
+  }))
+
+```
+
+];
+
+# ------------------------------------------------------------
+
+# Fonts
+
+# ------------------------------------------------------------
+
+fonts = {
+packages = with pkgs; [
+cantarell-fonts
+jetbrains-mono
+
+```
+  nerd-fonts.jetbrains-mono
+  nerd-fonts.fira-code
+  nerd-fonts.iosevka
+];
+
+fontconfig = {
+  enable = true;
+
+  defaultFonts = {
+    monospace = [ "JetBrains Mono" ];
+    sansSerif = [ "Cantarell" ];
+    serif = [ "Noto Serif" ];
   };
+};
+```
 
+};
 
-  boot.kernelParams = [
-      "i915.enable_psr=0"
-  ];
+# ------------------------------------------------------------
 
+# State version
 
-  service.displayManager.sddm = { 
-     enable = true;
-     wayland.enable = true; 
-  };
+# ------------------------------------------------------------
 
-
-#flakes-setting
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  networking.hostName = "transcendent"; # Define your hostname.
-
-# Enable networking
-    networking.networkmanager.enable = true;
-
-# Set your time zone.
-  time.timeZone = "Asia/Kolkata";
-
-# Select internationalisation properties.
-  i18n.defaultLocale = "en_IN";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_IN";
-    LC_IDENTIFICATION = "en_IN";
-    LC_MEASUREMENT = "en_IN";
-    LC_MONETARY = "en_IN";
-    LC_NAME = "en_IN";
-    LC_NUMERIC = "en_IN";
-    LC_PAPER = "en_IN";
-    LC_TELEPHONE = "en_IN";
-    LC_TIME = "en_IN";
-  };
-
-# Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-
-#flatpak-apps
-  services.flatpak.enable = true;
-
-
-# Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.isandrin = {
-    isNormalUser = true;
-    description = "isandrin";
-    extraGroups = [ "networkmanager" "wheel" "docker" "libvirtd" "kvm" ];
-    packages = with pkgs; [];
-  };
-
-# Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-# List packages installed in system profile. To search, run:
-# $ nix search wget
-  environment.systemPackages = with pkgs; [
-
-      git
-      ffmpegthumbnailer
-      lm_sensors
-      libnotify
-      zen-browser.packages.${pkgs.system}.default
-      bc
-      cliphist
-      glib
-      gsettings-desktop-schemas
-      stow
-      neovim
-      gcc 
-      gnumake
-      cmake
-      gdb
-      curl 
-      wget 
-      unzip 
-      adwaita-icon-theme
-      docker
-      tmux
-      wl-clipboard
-      brightnessctl
-      networkmanagerapplet
-      aria2
-      ];
-
-#fonts
-  fonts = {
-    packages = with pkgs; [
-      nerd-fonts.jetbrains-mono
-        cantarell-fonts 
-        jetbrains-mono
-        nerd-fonts.fira-code
-        nerd-fonts.iosevka
-    ];
-    fontconfig = {
-      enable = true;
-      defaultFonts = {
-        monospace = [ "JetBrains Mono" ];
-        sansSerif = [ "Cantarell" ];
-        serif = [ "Noto Serif" ];
-      };
-    };
-  };
-
-
-#powersaving tlp
-  services.tlp.enable = true;
-
-
-#acpi-thing-block
-  services.acpid.enable = true;
-
-
-#keyd-program-block
-  services.keyd = {
-    enable = true;
-
-    keyboards.default = {
-      ids = [ "*" ];
-      settings = {
-        main = {
-          rightalt = "leftmeta";
-        };
-      };
-    };
-  };
-
-
-#bluetooth-block
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true; 
-
-  systemd.user.services.blueman-applet = { 
-    enable = false; 
-  };
-
-#audio-stack(don't forget)
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-
-  security.rtkit.enable = true;
-
-  system.stateVersion = "25.11";
+system.stateVersion = "25.11";
 }
